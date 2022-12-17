3cb378448ee837b6c4e67e359d307e593c3b025f
Elias <borrajo.elias@gmail.com>
2022-12-17 16:21:08 +0100

    Création CSS & Fonction JS pour API

--- scripts/activitySearch.js
+++ scripts/activitySearch.js
@@ -2,8 +2,28 @@ console.log("activitySearch.js loaded");
 
 
 // This function is called when the user clicks the search button
+/*
+This code will get the value of the city input field when the button is clicked, 
+and use it to make an HTTP GET request to the specified URL. 
+The response from the server will be passed to the response => response.json() function, 
+which will parse the response as a JSON object. 
+The resulting data will be passed to the data => function, where you can do something with it. 
+*/
 function searchActivity() {
     console.log("searchActivity() called");
 
+    // Get the value of the input field
+    const city = document.getElementById('city').value;
 
+    // Make the HTTP request using the fetch function
+    fetch(`https://api.example.com/cities/${city}/activities`)
+    .then(response => response.json())
+    .then(data => {
+        // Do something with the data here
+        console.log(data);
+        })
+    .catch(error => {
+        // Handle any errors here
+        console.error(error);
+    });
 }
