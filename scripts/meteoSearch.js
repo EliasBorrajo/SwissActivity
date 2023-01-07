2c561a6702aa1f51f544c5c661c8bed57630f3c3
Milena <milenalonfat@gmail.com>
2023-01-07 09:13:00 +0100

    meteo

---
+++ scripts/meteoSearch.js
@@ -0,0 +1,25 @@
+console.log("meteoSearch.js loaded");
+
+// This function is called when the user clicks the search button
+
+const API_KEY = '6842a7cfc89801efb1369dfdf93e416c';
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+
+function searchMeteo() {
+    console.log("searchMeteo() called");
+
+
+    // Get the value of the input field
+    const city = document.getElementById('city').value;
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`)
+    .then(response => response.json())
+    .then(data => {
+        // ici, vous pouvez utiliser les données météo
+        console.log(data);
+    })
+    .catch(error => {
+        // Handle any errors here
+        console.error(error);
+    });
+}
