7c17ec7d765094ba8316d212bd5d37ca1604568a
Jonathan <jon.bourquin@gmail.com>
2023-01-07 08:26:41 +0100

    test API

--- scripts/activitySearch.js
+++ scripts/activitySearch.js
@@ -2,6 +2,16 @@ console.log("activitySearch.js loaded");
 
 
 // This function is called when the user clicks the search button
-function search() {
-    //call API
+function searchActivity() {
+    var searchCriteria = document.getElementById("city").value;
+    fetch('https://opendata.myswitzerland.io/v1/destinations?query=Lausanne', {
+  method: 'GET',
+  headers: {
+    'Accept': 'application/json',
+    'x-api-key': 'AlQbq7i0XYQXiSz2i0Gn1ibEqCQfasT9UyP9Xhdd'
+  }
+})
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error(error))
 }
